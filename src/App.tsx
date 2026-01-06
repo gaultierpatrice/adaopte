e5c4765a4f63cdf9bdec7cef7702d135cc9b3668
marineags <agsmarine23@gmail.com>
2026-01-06 17:06:29 +0100

    creat Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import LandingPage from "./pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
+import ListingPage from "./pages/ListingPage.tsx";
+import Navbar from "./components/NavBar";
+import LandingPage from "./pages/LandingPage.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/listing" element={<ListingPage />} />
+      </Routes>
+
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,14 +1,9 @@
-import Navbar from "../components/NavBar";
-import Footer from "../components/Footer";
-
 function LandingPage() {
   return (
     <>
-      <Navbar />
       <main className="min-h-[650px] flex flex-col items-center justify-center bg-pink-300">
         <h1 className="text-3xl font-bold underline">Adaopte</h1>
       </main>
-      <Footer />
     </>
   );
 }
