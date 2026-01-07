02dce0eb6aa1b8d21d55549d3d820657d07b2907
Gaultier Patrice <gaultiercode@gmail.com>
2026-01-07 10:37:05 +0100

    navbar ok with icons and spacing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,45 @@
 import Button from "./Button.tsx";
-
 import { NavLink } from "react-router-dom";
+import { CiHome } from "react-icons/ci";
+import {
+  LuBone,
+  LuFish,
+  LuSquirrel,
+  LuHandHeart,
+  LuPawPrint,
+} from "react-icons/lu";
 
 function NavBar() {
   return (
     <header className="flex flex-row justify-between p-5">
-      <h1>
-        Ada<span className="text-[--secondary-color]">opte</span>
+      <h1 className="flex items-center font-bold gap-2">
+        <span>
+          <LuPawPrint />
+        </span>
+        <span>
+          Ada<span className="text-(--secondary-color)">opte</span>
+        </span>
       </h1>
-      <NavLink to="/">Acceuil</NavLink>
-      <NavLink to="/listing">J'adopte</NavLink>
-      <a>Guide de l'adoption</a>
-      <a>Devenir bénévole</a>
-      <Button>Faire un don</Button>
+      <NavLink to="/" className="flex items-center gap-2">
+        <CiHome />
+        <span>Acceuil</span>
+      </NavLink>
+      <NavLink to="/listing" className="flex items-center gap-2">
+        <LuBone />
+        <span>J'adopte</span>
+      </NavLink>
+      <a className="flex items-center gap-2">
+        <LuFish />
+        <span>Guide de l'adoption</span>
+      </a>
+      <a className="flex items-center gap-2">
+        <LuSquirrel />
+        <span>Devenir bénévole</span>
+      </a>
+      <Button className="flex items-center gap-2">
+        <LuHandHeart />
+        Faire un don
+      </Button>
     </header>
   );
 }
