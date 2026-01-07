8ad1911df29f14e102bc0595fe18351aa5058086
marineags <agsmarine23@gmail.com>
2026-01-07 16:10:58 +0100

    update card adoption page

---
+++ src/components/Card.tsx
@@ -0,0 +1,66 @@
+interface CardProps {
+  name?: string;
+  age?: string;
+  breed?: string;
+  type?: string;
+  city?: string;
+  zipcode?: string;
+  description?: string;
+  imageUrl?: string;
+  imageAlt?: string;
+}
+
+function Card({
+  name,
+  age,
+  breed,
+  type,
+  city,
+  zipcode,
+  description,
+  imageUrl,
+  imageAlt,
+}: CardProps) {
+  return (
+    <div
+      className="card
+ w-full max-w-xs bg-white shadow-md"
+    >
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={imageAlt ?? ""}
+          className="w-full h-56 object-cover"
+        />
+      )}
+      {type && (
+        <p className="text-sm font-semibold text-gray-500 px-5 pt-4">{type}</p>
+      )}
+      {name && (
+        <h1 className="mt-1 text-2xl font-extrabold text-indigo-600 px-5">
+          {name}
+        </h1>
+      )}
+      {age && breed && (
+        <p className="mt-2 text-sm font-semibold text-gray-600 px-5">
+          {age} &bull; {breed}
+        </p>
+      )}
+
+      {description && (
+        <p className="mt-4 text-sm text-gray-600 leading-relaxed px-5 ">
+          {description}
+        </p>
+      )}
+      {city && (
+        <p className="mt-2 text-sm font-semibold text-black px-5">{city}</p>
+      )}
+      {zipcode && (
+        <p className="mt-2 text-sm font-semibold text-black px-5 pb-4">
+          Code postal : {zipcode}
+        </p>
+      )}
+    </div>
+  );
+}
+export default Card;
