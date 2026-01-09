14ec4ff7a77e88dd1cf988ea2970ed0a475aff30
marineags <agsmarine23@gmail.com>
2026-01-09 11:45:16 +0100

    creat  card landingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import Card from "../components/Card.tsx";
 import { NavLink } from "react-router-dom";
 import { LuHandHeart } from "react-icons/lu";
 import animals from "../data/data.json";
+import information from "../data/information.json";
 
 import charlieimg from "../pictures/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg";
 import miaimg from "../pictures/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg";
@@ -57,9 +58,28 @@ function LandingPage() {
             </NavLink>
           </section>
 
-          <h2 className="flex flex-col justify-center min-h-[650px] text-3xl font-bold underline">
-            Placeholder Comment ca marche
+          <h2 className="flex flex-col justify-center pt-12 text-3xl font-bold ">
+            Comment ça marche
           </h2>
+          <p className="text-sm px-50 pt-8 text-gray-600 ">
+            Adopter un animal est une décision importante et responsable. Pour
+            vous accompagner, nous avons mis en
+          </p>
+          <p className="text-sm px-50 mb-5 text-gray-600 ">
+            place un parcours d’adoption simple et transparent, en quelques
+            étapes.
+          </p>
+          <div className="grid grid-cols-4 gap-6 p-8 ">
+            {information.map((information, index) => (
+              <Card
+                key={index}
+                {...information}
+                imageUrl={imageMap[information.title] ?? information.imageUrl}
+                imageSize="small"
+                showButton={false}
+              />
+            ))}
+          </div>
           <div className="flex flex-col items-center justify-around bg-gray-100 min-h-[300px] w-full">
             <h1 className="text-5xl mt-5">Prêt.e à changer une vie ?</h1>
             <p className="text-sm px-50">
